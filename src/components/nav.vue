985aee3949c72c0dbd34cce84f21a93dc96a1f8c
Ishirai <kirakiseki@163.com>
2022-03-19 14:20:55 +0800

    chaos: Update banner

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -43,9 +43,15 @@
     </el-row>
 </template>
 
-<script>
+<script lang="ts" setup>
+import { ref } from 'vue'
 
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
 </script>
+
 <style scoped lang="scss">
 @import "@/assets/scss/nav.scss";
 </style>
