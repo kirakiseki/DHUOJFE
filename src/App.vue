6c8697f4c7c2fe75c6d9a9991ac7c43209946995
Ishirai <kirakiseki@163.com>
2022-03-22 21:06:08 +0800

    chaos: update layout

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,16 @@ import Nav from './components/nav.vue';
 </script>
 
 <template>
-  <Nav></Nav>
+  <el-container>
+    <el-header class="header">
+      <Nav></Nav>
+    </el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
 </template>
 
 <style lang="scss">
-@import '@/assets/scss/globalStyle.scss';
+@import "@/assets/scss/globalStyle.scss";
+@import "@/assets/scss/base.scss"
 </style>
-
