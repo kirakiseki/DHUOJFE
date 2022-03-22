a30c9f426a7d5944ce30c6dd63ff028b10ef54e8
Ishirai <kirakiseki@163.com>
2022-03-22 21:51:27 +0800

    feat: add affix for long pages

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import Nav from './components/nav.vue';
     <el-header class="header">
       <Nav></Nav>
     </el-header>
-    <el-main>Main</el-main>
+    <el-main class="main">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</el-main>
     <el-footer>Footer</el-footer>
   </el-container>
 </template>
--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,7 +1,6 @@
 <template>
     <el-row class="hidden-sm-and-down">
-        <el-col :span="2" class="banner banner-logo hidden-md-and-down">
-        </el-col>
+        <el-col :span="2" class="banner banner-logo hidden-md-and-down"></el-col>
         <el-col :span="22" class="banner hidden-md-and-down">
             <div class="banner-title-container">
                 <el-row>
@@ -13,34 +12,36 @@
             </div>
         </el-col>
     </el-row>
-    <el-row>
-        <el-col :span="24">
-            <el-menu
-                :default-active="activeIndex"
-                class="el-menu-demo"
-                mode="horizontal"
-                @select="handleSelect"
-            >
-                <p class="menu-title">DHU OJ</p>
-                <el-menu-item index="1">Home</el-menu-item>
-                <el-sub-menu index="2">
-                    <template #title>Problem Set</template>
-                    <el-menu-item index="2-1">item one</el-menu-item>
-                    <el-menu-item index="2-2">item two</el-menu-item>
-                    <el-menu-item index="2-3">item three</el-menu-item>
-                    <el-sub-menu index="2-4">
-                        <template #title>item four</template>
-                        <el-menu-item index="2-4-1">item one</el-menu-item>
-                        <el-menu-item index="2-4-2">item two</el-menu-item>
-                        <el-menu-item index="2-4-3">item three</el-menu-item>
+    <el-affix position="top">
+        <el-row>
+            <el-col :span="24">
+                <el-menu
+                    :default-active="activeIndex"
+                    class="el-menu-demo"
+                    mode="horizontal"
+                    @select="handleSelect"
+                >
+                    <p class="menu-title">DHU OJ</p>
+                    <el-menu-item index="1">Home</el-menu-item>
+                    <el-sub-menu index="2">
+                        <template #title>Problem Set</template>
+                        <el-menu-item index="2-1">item one</el-menu-item>
+                        <el-menu-item index="2-2">item two</el-menu-item>
+                        <el-menu-item index="2-3">item three</el-menu-item>
+                        <el-sub-menu index="2-4">
+                            <template #title>item four</template>
+                            <el-menu-item index="2-4-1">item one</el-menu-item>
+                            <el-menu-item index="2-4-2">item two</el-menu-item>
+                            <el-menu-item index="2-4-3">item three</el-menu-item>
+                        </el-sub-menu>
                     </el-sub-menu>
-                </el-sub-menu>
-                <el-menu-item index="3">Contest</el-menu-item>
-                <el-menu-item index="4">Blogs</el-menu-item>
-                <el-menu-item index="5">Info</el-menu-item>
-            </el-menu>
-        </el-col>
-    </el-row>
+                    <el-menu-item index="3">Contest</el-menu-item>
+                    <el-menu-item index="4">Blogs</el-menu-item>
+                    <el-menu-item index="5">Info</el-menu-item>
+                </el-menu>
+            </el-col>
+        </el-row>
+    </el-affix>
 </template>
 
 <script lang="ts" setup>
@@ -48,7 +49,7 @@ import { ref } from 'vue'
 
 const activeIndex = ref('1')
 const handleSelect = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
+    console.log(key, keyPath)
 }
 </script>
 
