7a7022bb5eaa54ee7d09a6894e3e21be3fa06366
Ishirai <kirakiseki@163.com>
2022-03-22 22:05:47 +0800

    feat: Add back-to-top button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Nav from './components/nav.vue';
+import Footer from './components/footer.vue';
 </script>
 
 <template>
@@ -7,12 +8,18 @@ import Nav from './components/nav.vue';
     <el-header class="header">
       <Nav></Nav>
     </el-header>
-    <el-main class="main">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</el-main>
-    <el-footer>Footer</el-footer>
+    <el-main class="main">
+      <el-skeleton :rows="15" />
+    </el-main>
+    <el-footer>
+      <p>Footer Area</p>
+      <Footer></Footer>
+    </el-footer>
   </el-container>
+  <el-backtop />
 </template>
 
 <style lang="scss">
 @import "@/assets/scss/globalStyle.scss";
-@import "@/assets/scss/base.scss"
+@import "@/assets/scss/base.scss";
 </style>
---
+++ src/components/footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <el-row class="hidden-sm-and-down">
+        <el-col :span="2" class="banner banner-logo hidden-md-and-down"></el-col>
+        <el-col :span="22" class="banner hidden-md-and-down">
+            <div class="banner-title-container">
+                <el-row>
+                    <span class="banner-title">DHU Online Judge</span>
+                </el-row>
+                <el-row>
+                    <p class="banner-subtitle">Developed by ACM Training Team of Donghua University</p>
+                </el-row>
+            </div>
+        </el-col>
+    </el-row>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/scss/nav.scss";
+</style>
